1b2e43bc4207e4cacb1af416f7068a54c3db44c0
lorenzo-damico <loredam@hotmail.it>
2020-08-05 17:15:37 +0200

    Primo snack

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -1,1 +1,20 @@
-alert("ciao");
+var primoNumero = parseInt(prompt("Inserisci il primo numero"));
+var secondoNumero= parseInt(prompt("Inserisci il secondo numero"));
+
+if (isNaN(primoNumero) || isNaN(secondoNumero)) {
+  alert("Sono ammessi solo valori numerici.");
+
+} else if (primoNumero > secondoNumero) {
+  var numeroStampato = primoNumero;
+  var messaggio = "Il numero maggiore è il primo: ";
+
+} else if (secondoNumero > primoNumero) {
+  var numeroStampato = secondoNumero;
+  var messaggio = "Il numero maggiore è il secondo: ";
+  
+} else {
+  var numeroStampato = primoNumero;
+  var messaggio = "I due numeri sono uguali: ";
+}
+
+document.getElementById("numero").innerHTML = messaggio + numeroStampato;
