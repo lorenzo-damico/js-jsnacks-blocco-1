0e07484aa2f71f66b8f4d04fdbc9d48559ab30f9
lorenzo-damico <loredam@hotmail.it>
2020-08-05 17:27:10 +0200

    Secondo snack

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,1 +1,12 @@
+var primaParola = prompt("Inserisci la prima parola");
+var secondaParola = prompt("Inserisci la seconda parola");
 
+if (primaParola.length < secondaParola.length) {
+  document.getElementById("parola").innerHTML = "La prima parola è più corta: " + primaParola + " " + secondaParola;
+
+} else if (secondaParola.length < primaParola.length) {
+  document.getElementById("parola").innerHTML = "La seconda parola è più corta: " + secondaParola + " " + primaParola;
+
+} else {
+  document.getElementById("parola").innerHTML = "Le due parole hanno la stessa lunghezza";
+}
